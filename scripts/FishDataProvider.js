6737fcec88132721760f725c2946892c943cdb7b
MegSemrad <mlsemrad753@gmail.com>
2020-11-17 15:14:40 -0600

    made fish objects in array in fishdataprodiver.js

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,42 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        species: "Spotted Sea Trout",
+        length: 14,
+        location: "Gulf of Mexico",
+        food: "crustaceans", 
+        image: ""
+        
+    },
+
+    {
+        name: "Bubbles",
+        species: "Beta",
+        length: 2.5,
+        location: "PetsMart",
+        food: "Beta pellets",
+        image: ""
+        
+    },
+
+    {
+        name: "Finn",
+        species: "Platnium Angel Fish",
+        length: 4,
+        location: "Freshwater River",
+        food: "Water Bugs",
+        image: ""
+        
+    },
+
+    {
+        name: "Nemo",
+        species: "Clownfish",
+        length: 3,
+        location: "Atlantic Ocean",
+        food: "Algae",
+        image: ""
+        
+    }
+  
+]
